ee604a495f232cb6740fe86161ac7cc76ed7f1c2
Caleb Mitiku <kalebmitiku55@gmail.com>
2026-01-29 17:55:08 +0300

    starting trades.js file

---
+++ trades.js
@@ -0,0 +1,36 @@
+class TradesPage {
+    constructor() {
+        this.currentPage = 1;
+        this.tradesPerPage = 10;
+        this.filteredTrades = [];
+        
+        this.init();
+    }
+    init() {
+        this.setupTheme();
+        this.setupMobileMenu();
+        this.loadTrades();
+        this.setupEventListeners();
+        this.populateFilters();
+        this.setupBackupRestore();
+    }
+setupTheme() {
+    const themeToggle = document.getElementById('themeToggle');
+    if (themeToggle) {
+        const savedTheme = localStorage.getItem('tradingJournalTheme');
+        if (savedTheme === 'dark') {
+            document.body.classList.add('dark-theme');
+            themeToggle.innerHTML = '<span class="icon icon-sun"></span>';
+        } else {
+            themeToggle.innerHTML = '<span class="icon icon-moon"></span>';
+        }
+        themeToggle.addEventListener('click', function() {
+            document.body.classList.toggle('dark-theme');
+            const isDark = document.body.classList.contains('dark-theme');
+            localStorage.setItem('tradingJournalTheme', isDark ? 'dark' : 'light');
+            this.innerHTML = isDark ? 
+                '<span class="icon icon-sun"></span>' : 
+                '<span class="icon icon-moon"></span>';
+        });
+    }
+}}
